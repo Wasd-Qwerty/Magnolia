310d139cac35ff04e5124c7c4f2f8ee5d35b3010
HRENADER <fadeev.fadeev2015@yandex.ru>
2023-03-29 18:32:27 +0300

    ебать я крутой

---
+++ parfume.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fontello.css">
+    <link rel="stylesheet" href="css/product.css">
+    <title>Парфюмерия</title>
+</head>
+<body>
+     <header>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                            <div class="container-fluid">
+                                <a class="navbar-brand" href="index.html">
+                                    <img src="image/logo.jpg" alt="" width="70" height="70">
+                                </a>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                                    aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                    <div class="navbar-nav">
+                                        <a class="nav-link" href="#">КОРЗИНА</a>
+                                        <a class="nav-link" href="about.html">О НАС</a>
+                                        <a class="nav-link" href="katalog.html">КАТАЛОГ</a>
+                                    </div>
+                                </div>
+                                <a class="navbar-brand" class="auth" href="#">
+                                    <img src="image/auth.png" alt="" width="45" height="45">
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+                <div class="wrapper">
+                <div class="product-img">
+                    <img src="https://fermer.ru/files/styles/attachmentpost/public/storage15oc/326056/img_20210120_220126.jpg?itok=wYf56UEJ" >
+                </div>
+                <div class="product-info">
+                    <div class="product-text">
+                    <h1>Помадка</h1>
+                    </div>
+                    <div class="product-price-btn">
+                    <p><span>5000000Р</span></p>
+                    <form action="form.php" target="_blank">
+                        <button >Купить</button>
+                    </form>
+                    </div>
+                </div>
+                </div>
+
+</body>
+</html>
